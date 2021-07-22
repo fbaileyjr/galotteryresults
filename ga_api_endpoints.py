0f0efcee8d617ca0865eebc67d51f63903c46a0e
Frank Bailey <frankbaileyjr@gmail.com>
2021-07-21 23:13:10 -0400

    Cleaned up the urls and get_search_results

--- ga_api_endpoints.py
+++ ga_api_endpoints.py
@@ -1,37 +1,29 @@
 # -*- coding: utf-8 -*-
 """Models for API requests & responses."""
 import dataclasses
+import datetime
 import json
 import re
 from datetime import date
 from time import sleep
 
-import ga_api_endpoints
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
-prefix_url = ""
 fantasy_5_url = "https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names=fantasy5&size=20&status=PAYABLE"
 mega_million_url = "https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names=mega&size=20&status=PAYABLE"
+powerball_url = "https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names=powerball&size=20&status=PAYABLE"
 
-fantasy_5 = {
-    "method": "POST",
-    "url": "api/v1",
-    "headers" : "",
-}
+# response.json()['draws'][0]['results'][0]['primary'] = list of results
+# response.json().get('draws')[0].get('results')[0].get('primary') = list of results
+#
+#time = response.json().get('draws')[0].get('drawTime')
+#date = datetime.datetime.fromtimestamp(basetime/1000)
+
 
 # might have to paginate 
 # dict_keys(['nextPageUrl', 'pageUrls', 'nextItems', 'previousItems', 'draws'])
 
-def get_search_results(response):
-    response = requests.request("GET", "https://atlanta.craigslist.org/search/jjj?query=office")
-    soup = BeautifulSoup(response.content, 'html.parser')
-    search_results = soup.find(id="search-results")
-    list_results = search_results.find_all(search_results, class_="result-row")
-    list_results[1].find("time").string
-    today = date.today()
-    today_craigslist = f"{day_dict[today.month]} {today.day}"
-    final_results = []
-    for result in list_results:
-        if result.find("time").string == today_craigslist:
-            final_results.append(result)
+def get_search_results(game, size):
+    url = f"https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names={game}&size={size}&status=PAYABLE"
+    response = requests.request("GET", url)
