315baa3c3b6649d25106930754f89fdb34ae92af
frankbaileyjr <frankbaileyjr@gmail.com>
2021-07-28 16:41:46 -0400

    finished with response/parsing. Add calculations.py for statistcal functions

--- ga_api_endpoints.py
+++ ga_api_endpoints.py
@@ -21,29 +21,57 @@ powerball_url = "https://www.galottery.com/api/v2/draw-games/draws/page?order=de
 # date = datetime.datetime.fromtimestamp(basetime/1000)
 # results_list = response.json().get('draws')[0].get('results')[0].get('primary')
 # f"{date.month:02d}"
-#for x in testlist:
+# for x in testlist:
 #    for key in x:
 #        for x in x[key]:
 #            print(x)
 
 # dict of game keys
-game_dict = { "Fantasy 5" : "fantasy5", "Mega Millions" : "mega", "PowerBall" : "poweball"}
+game_dict = {"Fantasy 5": "fantasy5",
+             "Mega Millions": "mega", "PowerBall": "powerball"}
 
-# might have to paginate 
+# might have to paginate
 # dict_keys(['nextPageUrl', 'pageUrls', 'nextItems', 'previousItems', 'draws'])
+
 
 def get_search_results(game, size):
     url = f"https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names={game}&size={size}&status=PAYABLE"
     response = requests.request("GET", url)
     return response
 
+
 def parse_fantasy_5(response):
     results_list = []
     for draw in response.json().get('draws'):
         time = draw.get('drawTime')
-        temp_date = datetime.datetime.fromtimestamp(time/1000)
+        temp_date = datetime.datetime.fromtimestamp(time / 1000)
         date = f"{temp_date.year}-{temp_date.month:02d}-{temp_date.day:02d}"
-        draw_results = { date : draw.get('results')[0].get('primary')}
+        draw_results = {date: draw.get('results')[0].get('primary')}
+        results_list.append(draw_results)
+    return results_list[::-1]
+
+
+def parse_mega(response):
+    results_list = []
+    for draw in response.json().get('draws'):
+        time = draw.get('drawTime')
+        temp_date = datetime.datetime.fromtimestamp(time / 1000)
+        date = f"{temp_date.year}-{temp_date.month:02d}-{temp_date.day:02d}"
+        draw_results = {date: {"primary": draw.get('results')[0].get(
+            'primary'), "megaball": draw.get('results')[0].get('secondary')[0]}}
+        results_list.append(draw_results)
+    return results_list[::-1]
+
+
+
+def parse_powerball(response):
+    results_list = []
+    for draw in response.json().get('draws'):
+        time = draw.get('drawTime')
+        temp_date = datetime.datetime.fromtimestamp(time / 1000)
+        date = f"{temp_date.year}-{temp_date.month:02d}-{temp_date.day:02d}"
+        draw_results = {date: {"primary": draw.get('results')[0].get(
+            'primary'), "powerball": draw.get('results')[0].get('secondary')[0]}}
         results_list.append(draw_results)
     return results_list[::-1]
 
