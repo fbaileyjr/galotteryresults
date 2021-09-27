b5f63ec8205e44d7db2aa22dbc1bf644c1703304
Frank Bailey <frankbaileyjr@gmail.com>
2021-09-27 10:49:46 -0400

    galottery changed their api...again

--- ga_api_endpoints.py
+++ ga_api_endpoints.py
@@ -7,12 +7,16 @@ import re
 from datetime import date
 from time import sleep
 
-import requests
 from bs4 import BeautifulSoup, SoupStrainer
+
+import requests
 
 fantasy_5_url = "https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names=fantasy5&size=20&status=PAYABLE"
 mega_million_url = "https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names=mega&size=20&status=PAYABLE"
 powerball_url = "https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names=powerball&size=20&status=PAYABLE"
+
+#https://www.galottery.com/api/v2/draw-games/draws/?game-names=FANTASY+5&status=OPEN&next-draws=96
+
 
 # response.json()['draws'][0]['results'][0]['primary'] = list of results
 # response.json().get('draws')[0].get('results')[0].get('primary') = list of results
@@ -27,56 +31,54 @@ powerball_url = "https://www.galottery.com/api/v2/draw-games/draws/page?order=de
 #            print(x)
 
 # dict of game keys
-game_dict = {"Fantasy 5": "fantasy5",
-             "Mega Millions": "mega", "PowerBall": "powerball"}
+game_dict = {'fantasy 5': 'FANTASY+5',
+             'mega millions': 'MEGA+MILLIONS', 'powerBall': 'POWERBALL'}
 
 # might have to paginate
 # dict_keys(['nextPageUrl', 'pageUrls', 'nextItems', 'previousItems', 'draws'])
 
-
+#class Endpoints():          
 def get_search_results(game, size):
-    url = f"https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws=180&game-names={game}&size={size}&status=PAYABLE"
+    url = f'https://www.galottery.com/api/v2/draw-games/draws/page?order=desc&previous-draws={size}&game-names={game}&size={size}&status=CLOSED'
     response = requests.request("GET", url)
     return response
 
-
 def parse_fantasy_5(response):
     first_list,second_list,third_list,fourth_list,fifth_list = [],[],[],[],[]
-    for draw in response.json().get('draws'):
-        #time = draw.get('drawTime')
-        #temp_date = datetime.datetime.fromtimestamp(time / 1000)
-        #date = f"{temp_date.year}-{temp_date.month:02d}-{temp_date.day:02d}"
-        if draw.get('results')[0].get('primary'):
-             first_list.append(draw.get('results')[0].get('primary')[0])
-             second_list.append(draw.get('results')[0].get('primary')[1])
-             third_list.append(draw.get('results')[0].get('primary')[2])
-             fourth_list.append(draw.get('results')[0].get('primary')[3])
-             fifth_list.append(draw.get('results')[0].get('primary')[4])
+    if isinstance(response.json().get('draws'), list):
+        for draw in response.json().get('draws'):
+            #time = draw.get('drawTime')
+            #temp_date = datetime.datetime.fromtimestamp(time / 1000)
+            #date = f"{temp_date.year}-{temp_date.month:02d}-{temp_date.day:02d}"
+            if draw.get('results')[0].get('primary'):
+                 first_list.append(draw.get('results')[0].get('primary')[0])
+                 second_list.append(draw.get('results')[0].get('primary')[1])
+                 third_list.append(draw.get('results')[0].get('primary')[2])
+                 fourth_list.append(draw.get('results')[0].get('primary')[3])
+                 fifth_list.append(draw.get('results')[0].get('primary')[4])
         draw_results = {"first" : first_list, "second" : second_list, "third" : third_list, "fourth" : fourth_list, "fifth" : fifth_list}
-    return draw_results
-
+        return draw_results
 
 def parse_mega(response):
     first_list,second_list,third_list,fourth_list,fifth_list,mega_list = [],[],[],[],[],[]
-    for draw in response.json().get('draws'):
-        # time = draw.get('drawTime')
-        # temp_date = datetime.datetime.fromtimestamp(time / 1000)
-        # date = f"{temp_date.year}-{temp_date.month:02d}-{temp_date.day:02d}"
-        if draw.get('results')[0].get('primary'):
-            first_list.append(draw.get('results')[0].get('primary')[0])
-            second_list.append(draw.get('results')[0].get('primary')[1])
-            third_list.append(draw.get('results')[0].get('primary')[2])
-            fourth_list.append(draw.get('results')[0].get('primary')[3])
-            fifth_list.append(draw.get('results')[0].get('primary')[4])
-        if draw.get('results')[0].get('secondary')[0]:
-            mega_list.append(draw.get('results')[0].get('secondary')[0])
+    if isinstance(response.json().get('draws'), list): 
+        for draw in response.json().get('draws'):
+            # time = draw.get('drawTime')
+            # temp_date = datetime.datetime.fromtimestamp(time / 1000)
+            # date = f"{temp_date.year}-{temp_date.month:02d}-{temp_date.day:02d}"
+            if draw.get('results')[0].get('primary'):
+                first_list.append(draw.get('results')[0].get('primary')[0])
+                second_list.append(draw.get('results')[0].get('primary')[1])
+                third_list.append(draw.get('results')[0].get('primary')[2])
+                fourth_list.append(draw.get('results')[0].get('primary')[3])
+                fifth_list.append(draw.get('results')[0].get('primary')[4])
+                # need to remove MB-
+                mega_list.append(draw.get('results')[0].get('primary')[6])
         draw_results = {
             "first" : first_list, "second" : second_list,"third" : third_list, 
             "fourth" : fourth_list, "fifth" : fifth_list, "mega" : mega_list
         }
     return draw_results
-
-
 
 def parse_powerball(response):
     first_list,second_list,third_list,fourth_list,fifth_list,pb_list = [],[],[],[],[],[]
@@ -90,8 +92,7 @@ def parse_powerball(response):
             third_list.append(draw.get('results')[0].get('primary')[2])
             fourth_list.append(draw.get('results')[0].get('primary')[3])
             fifth_list.append(draw.get('results')[0].get('primary')[4])
-        if draw.get('results')[0].get('secondary')[0]:
-            pb_list.append(draw.get('results')[0].get('secondary')[0])
+            pb_list.append(draw.get('results')[0].get('primary')[6])
         draw_results = {
             "first" : first_list, "second" : second_list,"third" : third_list, 
             "fourth" : fourth_list, "fifth" : fifth_list, "powerball" : pb_list
@@ -100,3 +101,20 @@ def parse_powerball(response):
 
 def jdump(obj):
     print(json.dumps(obj, indent=2))
+
+
+while True:
+    choice = input(f'Which game do you want? Fantasy 5, Mega Millions, or Powerball? ')
+    if choice.lower() in ['fantasy 5', 'mega millions', 'powerball']:
+        break
+    else:
+        print(f'Please provide a valid game selection.')
+
+response = get_search_results(game_dict.get(choice) , 20)
+
+if choice.lower() == 'fantasy 5':
+    results = parse_fantasy_5(response)
+elif choice.lower() == 'mega millions':
+    results = parse_mega(response)
+else:
+    results = parse_powerball(response)   
--- galotteryresults.py
+++ galotteryresults.py
@@ -3,137 +3,31 @@ import re
 from datetime import date
 from time import sleep
 
-import ga_api_endpoints
-import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
-day_dict = {
-    1: "Jan",
-    2: "Feb",
-    3: "Mar",
-    4: "Apr",
-    5: "May",
-    6: "Jun",
-    7: "Jul",
-    8: "Aug",
-    9: "Sep",
-    10: "Oct",
-    11: "Nov",
-    12: "Dec",
-}
-
-
-def get_info(url="athensga.craigslist.org"):
-    job_url = f"{url}/search/jjj?query=voice+over+%7C+voice+jobs+%7C+voice+wanted+%7C++voiceovers+%7C+voice+talent+%7C+voice+recordings"
-    gig_url = f"{url}/search/ggg?query=voice+over+%7C+voice+jobs+%7C+voice+wanted+%7C++voiceovers+%7C+voice+talent+%7C+voice+recordings"
-    payload = {}
-    headers = {
-        'Cookie': 'cl_b=4|93fc8594e7b76f0c01a65a2c12434158c5a57e95|1625772264ONi0U'}
-    job_response = requests.request("GET", job_url)
-    gig_response = requests.request("GET", gig_url)
-    results = {"jobs": job_response, "gig": gig_response}
-    return results
-    '''
-    get all elements for <ul class="rows" id="search-results">
-    iterate each list <li class="result-row" data-pid="7349349885">
-    if date equals to current date, then add URL (, a href) to list
-        <time class="result-date" datetime="2021-07-11 05:11" title="Sun 11 Jul 05:11:36 AM">Jul 11</time>
-        <a href="https://losangeles.craigslist.org/sfv/tlg/d/van-nuys-auditions-actor-singers-for/7349349885.html" class="result-image gallery empty"></a>
-
-   
-    '''
-
-
-def get_list_of_us_cities():
-    results = requests.request("GET", "https://geo.craigslist.org/iso/us")
-    site = "geo-site-list-container"
-    soup = BeautifulSoup(results.content, parse_only=SoupStrainer('a'))
-    sitelinks = []
-    craigslist_regex = "https:\/\/\w+\.craigslist\.org"
-    for link in soup:
-        if link.has_attr('href') and re.match(craigslist_regex, link['href']) and not re.match("https:\/\/(www|forums)\.craigslist\.org", link['href']):
-            sitelinks.append(link['href'])
-    return sitelinks
-
-
-def get_search_results(response):
-    response = requests.request(
-        "GET", "https://atlanta.craigslist.org/search/jjj?query=office")
-    soup = BeautifulSoup(response.content, 'html.parser')
-    search_results = soup.find(id="search-results")
-    list_results = search_results.find_all(search_results, class_="result-row")
-    list_results[1].find("time").string
-    today = date.today()
-    today_craigslist = f"{day_dict[today.month]} {today.day}"
-    final_results = []
-    for result in list_results:
-        if result.find("time").string == today_craigslist:
-            final_results.append(result)
-    # need to parse out the rest of the info and insert as key/value pairs, then append to final_results
-    '''
-    <li class="result-row" data-pid="7350278589" data-repost-of="7321752389">
-    <a class="result-image gallery" data-ids="3:00808_aymhwYCrVkJz_0wg0ka" href="https://atlanta.craigslist.org/atl/ofc/d/decatur-administrative-assistant/7350278589.html">
-    </a>
-    <div class="result-info">
-    <span class="icon icon-star" role="button">
-    <span class="screen-reader-text">favorite this post</span>
-    </span>
-    <time class="result-date" datetime="2021-07-13 07:10" title="Tue 13 Jul 07:10:14 AM">Jul 13</time>
-    <h3 class="result-heading">
-    <a class="result-title hdrlnk" data-id="7350278589" href="https://atlanta.craigslist.org/atl/ofc/d/decatur-administrative-assistant/7350278589.html" id="postid_7350278589">Administrative Assistant</a>
-    </h3>
-    <span class="result-meta">
-    <span class="result-hood"> (Decatur)</span>
-    <span class="result-tags">
-    <span class="pictag">pic</span>
-    </span>
-    <span class="banish icon icon-trash" role="button">
-    <span class="screen-reader-text">hide this posting</span>
-    </span>
-    <span aria-hidden="true" class="unbanish icon icon-trash red" role="button"></span>
-    <a class="restore-link" href="#">
-    <span class="restore-narrow-text">restore</span>
-    <span class="restore-wide-text">restore this posting</span>
-    </a>
-    </span>
-    </div>
-    </li>
-    '''
-    pass
+import ga_api_endpoints as endpoints
+import requests
 
 
 def jdump(obj):
     print(json.dumps(obj, indent=2))
 
 
-response = get_info("https://athensga.craigslist.org")
+if __name__ == "__main__":
+    while True:
+        choice = input(f'Which game do you want? Fantasy 5, Mega Millions, or Powerball? ')
+        if choice.lower() in ['fantasy 5', 'mega millions', 'powerball']:
+            break
+        else:
+            print(f'Please provide a valid game selection.')
 
+    response = endpoints.get_search_results(choice, 20)
 
-'''
-Here's the plan:
-first  - figure out what the url is to search for each jobs listing
+    game_choice = {'fantasy 5' : 'parse_fantasy_5', 'mega millions': 'parse_mega', 'powerball' : 'parse_powerball'}
 
-if no results, pass
-no results: 
-
-<div class="content" id="sortable-results">
-                <section class="favlistsection">
-                    <section class="favlistinfo">
-                    </section>
-                    <section class="banishlistinfo">
-                    </section>
-                </section>
-
-
-                    
-
-                    <div class="alert alert-sm alert-warning">Nothing found for that search. (All words must match.)
-</div>
-
-second - figure out how to format the return data
-third - get list of all cities
-fourth - iterate through all cities to search 
-fifth - display results? email?
-
-
-'''
+    if choice.lower() == 'fantasy 5':
+        results = endpoints.parse_fantasy_5(response)
+    elif choice.lower() == 'mega millions':
+        results = endpoints.parse_mega(response)
+    else:
+        results = endpoints.parse_powerball(response)   
